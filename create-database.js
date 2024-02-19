282688e634ea098d1337264b195715fbf39d80de
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-02-19 15:43:27 +0100

    created data

--- create-database.js
+++ create-database.js
@@ -1,108 +1,126 @@
 import mongoose, { connect } from "mongoose";
 
-const con = await connect(
-  "mongodb://localhost:27017/product-management-system"
-);
+const createDatabase = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://localhost:27017/product-management-system"
+    );
+    const { db } = mongoose.connection;
 
-const { db } = mongoose.connection;
+    const categorySchema = mongoose.Schema({
+      categoryName: String,
+      catergoryType: String,
+    });
 
-const categoryCol = await db.collection("category");
-const productCol = await db.collection("products");
-const offersCol = await db.collection("offers");
-const suppliersCol = await db.collection("suppliers");
-const ordersCol = await db.collection("orders");
+    const offersSchema = mongoose.Schema({
+      Products: String,
+      Price: String,
+      Active: Boolean,
+    });
 
-const categorySchema = mongoose.Schema({
-  categoryName: String,
-  catergoryType: String,
-});
+    const suppliersSchema = mongoose.Schema({
+      Name: String,
+      Contact: String,
+    });
 
-const offersSchema = mongoose.Schema({
-  Products: String,
-  Price: String,
-  Active: Boolean,
-});
+    const productSchema = mongoose.Schema({
+      Namee: String,
+      Category: String,
+      Price: Number,
+      Cost: Number,
+      Stock: Number,
+    });
 
-const suppliersSchema = mongoose.Schema({
-  Name: String,
-  Contact: String,
-});
+    const ordersSchema = mongoose.Schema({
+      Offer: String,
+      Quntity: Number,
+      Status: String,
+    });
 
-const productSchema = mongoose.Schema({
-  Namee: String,
-  Category: String,
-  Price: Number,
-  Cost: Number,
-  Stock: Number,
-});
+    const categoryModel = mongoose.model("Category", categorySchema);
+    const offerModel = mongoose.model("Offer", offersSchema);
+    const supplierModel = mongoose.model("Supplier", suppliersSchema);
+    const productModel = mongoose.model("Product", productSchema);
+    const orderModel = mongoose.model("Order", ordersSchema);
 
-const ordersSchema = mongoose.Schema({
-  Offer: String,
-  Quntity: Number,
-  Status: String,
-});
+    const categoryCol = await db.collection("category");
+    const productCol = await db.collection("products");
+    const offersCol = await db.collection("offers");
+    const suppliersCol = await db.collection("suppliers");
+    const ordersCol = await db.collection("orders");
 
-productcol.insertMany([
-  {
-    Namee: "Laptop",
-    Category: "Electronics",
-    Price: 1000,
-    Cost: 800,
-    Stock: 50,
-  },
-  {
-    Namee: "Smartphone",
-    Category: "Electronics",
-    Price: 800,
-    Cost: 600,
-    Stock: 40,
-  },
-  { Namee: "T-shirt", Category: "Clothing", Price: 20, Cost: 10, Stock: 100 },
-  {
-    Namee: "Refrigerator",
-    Category: "Home Appliances",
-    Price: 1200,
-    Cost: 1000,
-    Stock: 30,
-  },
-  {
-    Namee: "Shampoo",
-    Category: "Beauty & Personal Care",
-    Price: 10,
-    Cost: 5,
-    Stock: 80,
-  },
-  {
-    Namee: "Soccer Ball",
-    Category: "Sports & Outdoors",
-    Price: 30,
-    Cost: 20,
-    Stock: 60,
-  },
-]);
+    let insertProducts = await productCol.insertMany([
+      {
+        Namee: "Laptop",
+        Category: "Electronics",
+        Price: 1000,
+        Cost: 800,
+        Stock: 50,
+      },
+      {
+        Namee: "Smartphone",
+        Category: "Electronics",
+        Price: 800,
+        Cost: 600,
+        Stock: 40,
+      },
+      {
+        Namee: "T-shirt",
+        Category: "Clothing",
+        Price: 20,
+        Cost: 10,
+        Stock: 100,
+      },
+      {
+        Namee: "Refrigerator",
+        Category: "Home Appliances",
+        Price: 1200,
+        Cost: 1000,
+        Stock: 30,
+      },
+      {
+        Namee: "Shampoo",
+        Category: "Beauty & Personal Care",
+        Price: 10,
+        Cost: 5,
+        Stock: 80,
+      },
+      {
+        Namee: "Soccer Ball",
+        Category: "Sports & Outdoors",
+        Price: 30,
+        Cost: 20,
+        Stock: 60,
+      },
+    ]);
 
-offerscol.insertMany([
-  { Products: "Laptop, Smartphone", Price: "1800", Active: true },
-  { Products: "T-shirt, Shampoo", Price: "30", Active: true },
-  {
-    Products: "Refrigerator, Smartphone, Soccer Ball",
-    Price: "1830",
-    Active: false,
-  },
-]);
+    let insertOffers = await offersCol.insertMany([
+      { Products: "Laptop, Smartphone", Price: "1800", Active: true },
+      { Products: "T-shirt, Shampoo", Price: "30", Active: true },
+      {
+        Products: "Refrigerator, Smartphone, Soccer Ball",
+        Price: "1830",
+        Active: false,
+      },
+    ]);
 
-supplierscol.insertMany([
-  {
-    Name: "Electronics Supplier Inc.",
-    Contact: "John Doe (john@electronicsupplier.com)",
-  },
-  {
-    Name: "Fashion Supplier Co.",
-    Contact: "Jane Smith (jane@fashionsupplier.com)",
-  },
-]);
+    let insertSuppliers = await suppliersCol.insertMany([
+      {
+        Name: "Electronics Supplier Inc.",
+        Contact: "John Doe (john@electronicsupplier.com)",
+      },
+      {
+        Name: "Fashion Supplier Co.",
+        Contact: "Jane Smith (jane@fashionsupplier.com)",
+      },
+    ]);
 
-orderscol.insertMany([
-  { Offer: "Offer 1", Quntity: 2, Status: "pending" },
-  { Offer: "Offer 3", Quntity: 1, Status: "pending" },
-]);
+    let insertOrders = await ordersCol.insertMany([
+      { Offer: "Offer 1", Quntity: 2, Status: "pending" },
+      { Offer: "Offer 3", Quntity: 1, Status: "pending" },
+    ]);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+createDatabase();
