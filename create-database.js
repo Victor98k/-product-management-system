620443f084d2d9264a800c5d2374a0f411546290
Victor98k <Victorgustav98@gmail.com>
2024-02-19 11:56:35 +0100

    test

--- create-database.js
+++ create-database.js
@@ -5,9 +5,3 @@ const con = await connect("");
 const { db } = mongoose.connection;
 
 const productCol = await db.collection("products");
-
-<<<<<<< HEAD:index.js
-
-daniel sjunger som en gud 
-=======
->>>>>>> 0857f40c08ed54410ac1453fc4c0c898caa421c8:create-database.js
