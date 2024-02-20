3fc9f9f2a6fd38458781102d694e971f06bdd44f
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-02-20 14:03:19 +0100

    Created structure for main.js

--- main.js
+++ main.js
@@ -46,6 +46,7 @@ const main = async () => {
     console.log("12. View suppliers");
     console.log("13.  View all sales");
     console.log("14.  View sum of all profits");
+    console.log("15. Exit application");
   } catch (error) {
     console.log(("Error fetching data:", error));
   }
@@ -57,332 +58,154 @@ const main = async () => {
     switch (prompt) {
       case "1":
         console.clear();
-        // Our method to find and display all of our movies from the database.
-        let movies = await movieModel.find({});
-        console.clear();
-
-        console.log(movies);
-        console.log("Above is the list of all current movies in the database.");
-        console.log("----------Menu----------");
-        console.log("1. Show all movies.");
-        console.log("2. Add new movie");
-        console.log(
-          "3. Update a movie (Update title, director or release date)"
-        );
-        console.log("4. Delete a movie.");
-        console.log("5. Exit");
-        console.log("-------------------------");
-
+        await addNewCategory();
         break;
 
       case "2":
         console.clear();
-
-        console.log("Add new movie");
-        console.log("Press 8 to go back to the main menu");
-        async function addMovie() {
-          let title = p("Enter new title: ");
-          let director = p("Enter a director: ");
-          let releaseYear = p(
-            "Enter releaseYear: (Enter the year in numbers) "
-          );
-          let genres = p("Enter genres: ");
-          let ratings = p("Enter ratings:  ");
-          let cast = p("Enter cast: ");
-          let newMovie = {
-            title,
-            director,
-            releaseYear,
-            genres,
-            ratings,
-            cast,
-          };
-          await movieModel.create(newMovie);
-          console.log(" You have added a new movie: ");
-          console.log(newMovie);
-        }
-        await addMovie();
-        console.log("----------Menu----------");
-        console.log("1. Show all movies.");
-        console.log("2. Add new movie");
-        console.log(
-          "3. Update a movie (Update title, director or release date)"
-        );
-        console.log("4. Delete a movie.");
-        console.log("5. Exit");
-        console.log("-------------------------");
+        await addNewProduct();
         break;
 
       case "3":
         console.clear();
-        console.log("You have chosen to update a movie.");
-        await updateMovie();
-
-        console.log("----------Menu----------");
-        console.log("1. Show all movies.");
-        console.log("2. Add new movie");
-        console.log(
-          "3. Update a movie (Update title, director or release date)"
-        );
-        console.log("4. Delete a movie.");
-        console.log("5. Exit");
-        console.log("-------------------------");
+        await viewProductsByCategory();
         break;
 
       case "4":
         console.clear();
-        console.log("You have choosen to delete a movie");
-        await deleteMovie();
-
+        await viewProductsBySupplier();
         break;
       case "5":
-        runApp = false;
+        console.clear();
+        await viewAllOffersInPriceRange();
+        break;
+      case "6":
+        console.clear();
+        await offersFromCategory();
+        break;
+      case "7":
+        console.clear();
+        await viewOffersBasedOnStock();
+        break;
+      case "8":
+        console.clear();
+        await createOrderForProducts();
+        break;
+      case "9":
+        console.clear();
+        await createOrderForOffers();
+        break;
+      case "10":
+        console.clear();
+        await shipOrders();
+        break;
+      case "11":
+        console.clear();
+        await addNewSupplier();
+        break;
+      case "12":
+        console.clear();
+        await viewAllSuppliers();
+        break;
+      case "13":
+        console.clear();
+        await viewAllSales();
+        break;
+      case "14":
+        console.clear();
+        await viewSumOfProfits();
+        break;
+      case "15":
+        console.clear();
+        // EXIT APP CODE
         break;
       default:
         console.log("Please enter a number between 1-5");
     }
   }
-
-  // Update movie
-  async function updateMovie() {
-    let movieTitle = p("Enter the title of the movie you want to update: ");
-    let movieToUpdate = await movieModel.findOne({ title: movieTitle });
-
-    let newTitle = "";
-    let newDirector = "";
-    let newReleaseYear = "";
-    let newGenres = [];
-    let newRatings = [];
-    let newCast = [];
-
-    if (movieToUpdate) {
-      console.log("--------------------------------------");
-      console.log("What would you like to update?");
-      console.log("1. Title");
-      console.log("2. Director");
-      console.log("3. Release Year ");
-      console.log("4. Genres");
-      console.log("5. Ratings");
-      console.log("6. Cast");
-      console.log("7. Update all");
-      console.log("8. Go back to the main menu");
-      console.log("--------------------------------------");
-
-      let updateChoice = p("Make a choice by entering number: ");
-
-      switch (updateChoice) {
-        case "1":
-          newTitle = p("Enter new title: ");
-          movieToUpdate.title = newTitle;
-          break;
-        case "2":
-          newDirector = p("Enter new director: ");
-          movieToUpdate.director = newDirector;
-          break;
-        case "3":
-          newReleaseYear = p("Enter new release year: ");
-
-          movieToUpdate.releaseYear = newReleaseYear;
-          break;
-        case "4":
-          let genreAction = p(
-            "Would you like to add or remove a genre? (add/remove): "
-          );
-          let genreName = p("Enter the genre name: ");
-          if (genreAction === "add") {
-            movieToUpdate.genres.push(genreName);
-          } else if (genreAction === "remove") {
-            movieToUpdate.genres = movieToUpdate.genres.filter(
-              (genre) => genre !== genreName
-            );
-          }
-          break;
-        case "5":
-          let ratingAction = p(
-            "Would you like to add or remove a rating? (add/remove): "
-          );
-          let ratingValue = p("Enter the rating value: ");
-          if (ratingAction === "add") {
-            movieToUpdate.ratings.push(ratingValue);
-          } else if (ratingAction === "remove") {
-            movieToUpdate.ratings = movieToUpdate.ratings.filter(
-              (rating) => rating !== ratingValue
-            );
-          }
-          break;
-        case "6":
-          let castAction = p(
-            "Would you like to add or remove a cast member? (add/remove): "
-          );
-          let castName = p("Enter the cast member name: ");
-          if (castAction === "add") {
-            movieToUpdate.cast.push(castName);
-          } else if (castAction === "remove") {
-            movieToUpdate.cast = movieToUpdate.cast.filter(
-              (cast) => cast !== castName
-            );
-          }
-          break;
-        case "7":
-          newTitle = p("Enter new title: ");
-          newDirector = p("Enter new director: ");
-          newReleaseYear = p("Enter new release year: ");
-          newGenres = p("Enter new genres: ");
-          newRatings = p("Enter new ratings: ");
-          newCast = p("Enter new cast: ");
-
-          movieToUpdate.title = newTitle;
-          movieToUpdate.director = newDirector;
-          movieToUpdate.releaseYear = newReleaseYear;
-          movieToUpdate.genres = newGenres;
-          movieToUpdate.ratings = newRatings;
-          movieToUpdate.cast = newCast;
-          break;
-
-        default:
-          console.log("Invalid choice. Please try again.");
-          break;
-      }
-
-      await movieToUpdate.save();
-      console.clear();
-      console.log("Movie updated successfully!");
-    } else {
-      console.log("Movie not found. Please try again.");
-    }
-  }
 };
 main();
 
-// Function to delete one movie from the  collection.
-async function deleteMovie() {
-  let movieToDelete = p(
-    "Please enter the title of the movie you want to delete: "
-  );
+async function addNewCategory() {
+  // Function to add new category
+  let name = p("Enter category name: ");
+  let catergoryType = p("Enter category type: ");
 
-  const deleteMovie = await movieModel.findOne({ title: movieToDelete });
-
-  if (!movieToDelete) {
-    console.log("No movie found with the provided title.");
-    return;
-  }
-
-  await movieModel.deleteOne({ _id: deleteMovie._id });
-  console.log("You have deleted the movie: " + movieToDelete);
-  console.log("----------Menu----------");
-  console.log("1. Show all movies.");
-  console.log("2. Add new movie");
-  console.log("3. Update a movie (Update title, director or release date)");
-  console.log("4. Delete a movie.");
-  console.log("5. Exit");
-  console.log("-------------------------");
+  let newCategory = {
+    name,
+    catergoryType,
+  };
+  await catergoryCol.create(newCategory);
+  console.log(" You have added a new category: ");
+  console.log(newCategory);
 }
 
-// Update movie
-async function updateMovie() {
-  let movieTitle = p("Enter the title of the movie you want to update: ");
-  let movieToUpdate = await movieModel.findOne({ title: movieTitle });
-
-  let newTitle = "";
-  let newDirector = "";
-  let newReleaseYear = "";
-  let newGenres = [];
-  let newRatings = [];
-  let newCast = [];
-
-  if (movieToUpdate) {
-    console.log("--------------------------------------");
-    console.log("What would you like to update?");
-    console.log("1. Title");
-    console.log("2. Director");
-    console.log("3. Release Year ");
-    console.log("4. Genres");
-    console.log("5. Ratings");
-    console.log("6. Cast");
-    console.log("7. Update all");
-    console.log("8. Go back to the main menu");
-    console.log("--------------------------------------");
-
-    let updateChoice = p("Make a choice by entering number: ");
-
-    switch (updateChoice) {
-      case "1":
-        newTitle = p("Enter new title: ");
-        movieToUpdate.title = newTitle;
-        break;
-      case "2":
-        newDirector = p("Enter new director: ");
-        movieToUpdate.director = newDirector;
-        break;
-      case "3":
-        newReleaseYear = p("Enter new release year: ");
-
-        movieToUpdate.releaseYear = newReleaseYear;
-        break;
-      case "4":
-        let genreAction = p(
-          "Would you like to add or remove a genre? (add/remove): "
-        );
-        let genreName = p("Enter the genre name: ");
-        if (genreAction === "add") {
-          movieToUpdate.genres.push(genreName);
-        } else if (genreAction === "remove") {
-          movieToUpdate.genres = movieToUpdate.genres.filter(
-            (genre) => genre !== genreName
-          );
-        }
-        break;
-      case "5":
-        let ratingAction = p(
-          "Would you like to add or remove a rating? (add/remove): "
-        );
-        let ratingValue = p("Enter the rating value: ");
-        if (ratingAction === "add") {
-          movieToUpdate.ratings.push(ratingValue);
-        } else if (ratingAction === "remove") {
-          movieToUpdate.ratings = movieToUpdate.ratings.filter(
-            (rating) => rating !== ratingValue
-          );
-        }
-        break;
-      case "6":
-        let castAction = p(
-          "Would you like to add or remove a cast member? (add/remove): "
-        );
-        let castName = p("Enter the cast member name: ");
-        if (castAction === "add") {
-          movieToUpdate.cast.push(castName);
-        } else if (castAction === "remove") {
-          movieToUpdate.cast = movieToUpdate.cast.filter(
-            (cast) => cast !== castName
-          );
-        }
-        break;
-      case "7":
-        newTitle = p("Enter new title: ");
-        newDirector = p("Enter new director: ");
-        newReleaseYear = p("Enter new release year: ");
-        newGenres = p("Enter new genres: ");
-        newRatings = p("Enter new ratings: ");
-        newCast = p("Enter new cast: ");
-
-        movieToUpdate.title = newTitle;
-        movieToUpdate.director = newDirector;
-        movieToUpdate.releaseYear = newReleaseYear;
-        movieToUpdate.genres = newGenres;
-        movieToUpdate.ratings = newRatings;
-        movieToUpdate.cast = newCast;
-        break;
-
-      default:
-        console.log("Invalid choice. Please try again.");
-        break;
-    }
-
-    await movieToUpdate.save();
-    console.clear();
-    console.log("Movie updated successfully!");
-  } else {
-    console.log("Movie not found. Please try again.");
+async function addNewProduct() {
+  // Function to add new product
+  console.log("Add new product");
+  async function addNewProduct() {
+    let Namee = p("Enter name of product");
+    let Category = p("Enter Category");
+    let Price = p("Enter Price");
+    let Cost = p("Enter Cost");
+    let Stock = p("Enter Stock");
   }
+  let newProduct = {
+    Namee,
+    Category,
+    Price,
+    Cost,
+    Stock,
+  };
+  await productModel.create(newProduct);
+  console.log("You have added a new product");
+  console.log(newProduct);
+}
+
+async function viewProductsByCategory() {
+  // Function to view products by category
+}
+
+async function viewProductsBySupplier() {
+  // Function to view products by supplier
+}
+
+async function viewAllOffersInPriceRange() {
+  // Function to view all offers in a specific price range
+}
+
+async function offersFromCategory() {
+  // Function to view all offers that contain a product from a specific category
+}
+
+async function viewOffersBasedOnStock() {
+  // Function to view the number of offers based on the number of its products in stock
+}
+
+async function createOrderForProducts() {
+  // Function to create order for individual products
+}
+
+async function createOrderForOffers() {
+  // Function to create order for offers
+}
+
+async function shipOrders() {
+  // Function to ship orders
+}
+
+async function addNewSupplier() {
+  // Function to add new supplier
+}
+
+async function viewAllSuppliers() {
+  // Function to view all suppliers
+}
+
+async function viewAllSales() {
+  // Function to view all sales
+}
+
+async function viewSumOfProfits() {
+  // Function to view the sum of all profits
 }
