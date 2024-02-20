e3407ac8c00022977a2383ea4d0f20af77ac1c7c
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-02-20 13:11:47 +0100

    edits

--- create-database.js
+++ create-database.js
@@ -3,7 +3,7 @@ import mongoose, { connect } from "mongoose";
 const createDatabase = async () => {
   try {
     await mongoose.connect(
-      "mongodb://localhost:27017/product-management-system"
+      "mongodb://127.0.0.1:27017/product-management-system"
     );
     const { db } = mongoose.connection;
 
